b21fb7bd8db5f65d2ac9282073396476a71307dc
Jens Feld <jens.feld@uniteplus.de>
2021-07-09 07:57:44 +0000

    feat: App, Header, MobileMenu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,16 +23,16 @@ const Main = styled.main`
   padding: 64px 32px;
 
   @media ${QUERIES.laptopAndSmaller} {
-    background: yellow;
+    /* background: yellow; */
   }
 
   @media ${QUERIES.tabletAndSmaller} {
-    background: red;
+    /* background: red; */
     padding: 48px 32px;
   }
 
   @media ${QUERIES.phoneAndSmaller} {
-    background: fuchsia;
+    /* background: fuchsia; */
     padding: 48px 16px;
   }
 `;
